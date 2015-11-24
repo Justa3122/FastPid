bc3694970a707028c000f75f0c74d1acc51bede2
Justyna Jasek <justyna.jasek.93@gmail.com>
2015-11-25 00:38:04 +0100

    game

---
+++ FastPid2/FastPid2/MainActivity.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Locations;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Android.Util;
+using Android.App;
+using Android.Gms.Maps;
+using Android.OS;
+using Android;
+
+namespace FastPid2
+{
+    [Activity(Label = "FastPid", MainLauncher = true, Icon = "@drawable/icon")]
+
+    public class MainActivity : Activity, IOnMapReadyCallback
+    {
+
+        private GoogleMap mMap;
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.Main);
+            SetUpMap();
+
+        }
+
+        private void SetUpMap()
+        {
+            if (mMap == null)
+            {
+                FragmentManager.FindFragmentById<MapFragment>(Resource.Id.map).GetMapAsync(this);
+            }
+        }
+        public void OnMapReady(GoogleMap googleMap)
+        {
+            mMap = googleMap;
+        }
+
+    }
+}
+
